dcd468d7a35bfd6a5ed96ff18454199b42c9495b
suenatu <nmhmpooh.sent11030@gmail.com>
2021-09-27 20:35:49 +0900

    Update randomkey_generation.py
    小文字、大文字、英語の集合を追加

--- randomkey_generation.py
+++ randomkey_generation.py
@@ -1,4 +1,8 @@
 small_alfabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'}
 big_alfabet = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'}
+number = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
 
-print(len(big_alfabet))
+print(len(number))
+
+# 必要な文字列を入力
+#number_count = int(input())
